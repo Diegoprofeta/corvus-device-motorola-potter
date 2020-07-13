51a66f82c2e70644d3b6f29d50b529da9ed708a3
Zilberto Junior <zjrdroid@pixelexperience.org>
2020-07-13 15:58:45 -0300

    set screen resolution for Bootanimation

--- du_potter.mk
+++ du_potter.mk
@@ -30,6 +30,7 @@ $(call inherit-product, device/motorola/potter/device.mk)
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
+scr_resolution := 1080
 IS_PHONE := true
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := potter
